384c75eafbe73dd01203df2a59520b018b047280
Nikita Konovalov <n.konovalov1990@gmail.com>
2025-03-21 18:45:22 +0100

    Выделенки справа

--- data.js
+++ data.js
@@ -1,5 +1,11 @@
 const PLANS = [
   {
+    title: "Выделенки справа",
+    map: "lines-right.jpg",
+    old: true,
+    default: true
+  },
+  {
     title: "Предложения",
     map: "project.jpg",
     old: true,
